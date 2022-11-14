f60eb9d6846572db78645698ff2bde1d28568d33
milton320 <torrezmilton8@gmail.com>
2022-11-14 13:51:11 -0400

    CRUD COMPLETO INTEGRACION DE DATA TABLES

--- resources/views/admin/personals/index.blade.php
+++ resources/views/admin/personals/index.blade.php
@@ -17,7 +17,7 @@
 @section('content')
     <div class="card">
         <div class="card-body">
-            <table class="table table-striped">
+            <table class="table table-striped" id="personal">
                 <thead>
                     <tr>
                     <th scope="col">#</th>
@@ -58,6 +58,10 @@
 
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
+    {{--Datatable css--}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.bootstrap4.min.css">
 @stop
 
 @section('js')
@@ -93,4 +97,25 @@
             })
         })
     </script>
-@stop
+    {{--Datatables--}}
+    <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.6/js/responsive.bootstrap4.min.js"></script><script>
+            $('#personal').DataTable({
+            responsive: true,
+            autowidth: false,
+            "language": {
+                "lengthMenu": "Mostrar _MENU_ registros por página",
+                "zeroRecords": "El registro no existe - disculpe",
+                "info": "Mostrando página _PAGE_ de _PAGES_",
+                "infoEmpty": "No records available",
+                "infoFiltered": "(filtrado de _MAX_ registros totales)",
+                "search": "Buscar:",
+                "paginate":{
+                    "next":"Siguiente",
+                    "previous":"Anterior"
+                }
+            }
+        });
+    </script>@stop
